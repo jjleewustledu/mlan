c976a9e6622e7bca6ef644ded126782b1dd46f2f
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-06-18 00:11:56 -0500

    tidying

--- src/+mlan/ProjectData.m
+++ src/+mlan/ProjectData.m
@@ -7,9 +7,6 @@ classdef ProjectData < mlnipet.ProjectData
  	%% It was developed on Matlab 9.5.0.1067069 (R2018b) Update 4 for MACI64.  Copyright 2019 John Joowon Lee.
  	
 	methods 
-        function g = getProjectFolder(varargin)
-            g = 'CCIR_00993';
-        end
         function p = session2project(varargin)
             %% e.g.:  {'CNDA_E1234','ses-E1234'} -> 'CCIR_00123'
             
@@ -21,6 +18,7 @@ classdef ProjectData < mlnipet.ProjectData
  			%  @param .
 
  			this = this@mlnipet.ProjectData(varargin{:});
+            this.projectFolder_ = 'CCIR_00993';
  		end
     end 
 
