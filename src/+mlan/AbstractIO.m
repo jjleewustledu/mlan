d9d075bf156c7752cc1e9b4a7bae1d871e8199e1
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2022-01-13 16:10:08 -0600

    implemented mlio.IOInterface, which is handle compatible

--- src/+mlan/AbstractIO.m
+++ src/+mlan/AbstractIO.m
@@ -1,4 +1,4 @@
-classdef AbstractIO < mlan.AbstractSimpleIO    
+classdef AbstractIO < mlio.AbstractIO    
 	%% ABSTRACTIO provides thin, minimalist methods for I/O.  Agnostic to all other object characteristics.
     
 	%  $Revision$
@@ -13,115 +13,7 @@ classdef AbstractIO < mlan.AbstractSimpleIO
         DEBUGGING = true
     end
     
-	properties (Dependent)
-        filename
-        filepath
-        fileprefix 
-        filesuffix
-        fqfilename
-        fqfileprefix
-        fqfn
-        fqfp
-        noclobber
-    end
-    
-    methods (Static)   
-    end
-    
-    methods 
-        
-        %% SET/GET
-        
-        function this = set.filename(this, fn)
-            assert(ischar(fn));
-            [this.filepath,this.fileprefix,this.filesuffix] = myfileparts(fn);
-        end
-        function fn   = get.filename(this)
-            fn = [this.fileprefix this.filesuffix];
-        end
-        function this = set.filepath(this, pth)
-            assert(ischar(pth));
-            this.filepath_ = pth;
-        end
-        function pth  = get.filepath(this)
-            if (isempty(this.filepath_))
-                this.filepath_ = pwd; 
-            end
-            pth = this.filepath_;
-        end
-        function this = set.fileprefix(this, fp)
-            assert(ischar(fp));
-            assert(~isempty(fp));
-            this.fileprefix_ = fp;
-        end
-        function fp   = get.fileprefix(this)
-            fp = this.fileprefix_;
-        end
-        function this = set.filesuffix(this, fs)
-            assert(ischar(fs));
-            if (~isempty(fs) && ~strcmp('.', fs(1)))
-                fs = ['.' fs];
-            end
-            [~,~,this.filesuffix_] = myfileparts(fs);
-        end
-        function fs   = get.filesuffix(this)
-            if (isempty(this.filesuffix_))
-                fs = ''; return; end
-            if (~strcmp('.', this.filesuffix_(1)))
-                this.filesuffix_ = ['.' this.filesuffix_]; end
-            fs = this.filesuffix_;
-        end
-        function this = set.fqfilename(this, fqfn)
-            assert(ischar(fqfn));
-            [p,f,e] = myfileparts(fqfn);
-            if (~isempty(p))
-                this.filepath = p;
-            end
-            if (~isempty(f))
-                this.fileprefix = f;
-            end
-            if (~isempty(e))
-                this.filesuffix = e;
-            end        
-        end
-        function fqfn = get.fqfilename(this)
-            fqfn = [this.fqfileprefix this.filesuffix];
-        end
-        function this = set.fqfileprefix(this, fqfp)
-            assert(ischar(fqfp));
-            [p,f] = fileprefixparts(fqfp);            
-            if (~isempty(p))
-                this.filepath = p;
-            end
-            if (~isempty(f))
-                this.fileprefix = f;
-            end
-        end
-        function fqfp = get.fqfileprefix(this)
-            fqfp = fullfile(this.filepath, this.fileprefix);
-        end
-        function this = set.fqfn(this, f)
-            this.fqfilename = f;
-        end
-        function f    = get.fqfn(this)
-            f = this.fqfilename;
-        end
-        function this = set.fqfp(this, f)
-            this.fqfileprefix = f;
-        end
-        function f    = get.fqfp(this)
-            f = this.fqfileprefix;
-        end
-        function this = set.noclobber(this, nc)
-            assert(islogical(nc));
-            this.noclobber_ = nc;
-        end
-        function tf   = get.noclobber(this) 
-            tf = this.noclobber_;
-        end
-        
-        %%
-        
+    methods
         function fid  = fopen(this, varargin)
             ip = inputParser;
             addOptional(ip, 'fn', this.fqfilename, @ischar);
@@ -135,12 +27,7 @@ classdef AbstractIO < mlan.AbstractSimpleIO
         end
         
         function this = AbstractIO(varargin)
-            ip = inputParser;
-            ip.KeepUnmatched = true;
-            addParameter(ip, 'noclobber', true, @islogical);
-            parse(ip, varargin{:});
-            
-            this.noclobber = ip.Results.noclobber;
+            this = this@mlio.AbstractIO(varargin{:});
         end
     end
     
@@ -158,7 +45,7 @@ classdef AbstractIO < mlan.AbstractSimpleIO
             bn = fullfile(pth, strtok(fp, '.'));
         end
         function nbytes = dprintf(meth, obj, varargin)
-            if (~mlan.AbstractIO.DEBUGGING)
+            if isempty(getenv('DEBUG'))
                 return
             end
             assert(ischar(meth));
---
+++ src/+mlan/MMRBids.m
@@ -0,0 +1,25 @@
+classdef MMRBids < handle & mlpipeline.Bids
+	%% MMRBIDS  
+
+	%  $Revision$
+ 	%  was created 13-Nov-2021 15:03:42 by jjlee,
+ 	%  last modified $LastChangedDate$ and placed into repository /Users/jjlee/MATLAB-Drive/mlan/src/+mlan.
+ 	%% It was developed on Matlab 9.11.0.1769968 (R2021b) for MACI64.  Copyright 2021 John Joowon Lee.
+ 	
+	properties
+ 		
+ 	end
+
+	methods 
+		  
+ 		function this = MMRBids(varargin)
+ 			%% MMRBIDS
+ 			%  @param .
+
+ 			
+ 		end
+ 	end 
+
+	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
+end
+
